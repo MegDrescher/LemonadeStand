fa133557f967d059f776cd9af14300f9eeb5e880
MegRyddner <44841685+MegRyddner@users.noreply.github.com>
2018-11-28 13:39:07 -0600

    Add files via upload

---
+++ LemonadeStand/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Game
+    {
+        Player player = new Player();
+        Inventory inventory = new Inventory();
+        Day day = new Day();
+        Weather weather = new Weather();
+        Recipe recipe = new Recipe();
+        BuyFromStore buyFromStore = new BuyFromStore();
+        Customer customer = new Customer();
+
+        public int dayNumber = 1;
+
+    }
+
+    public void 
+    {
+       
+
+    }
+}
